e24632a297ab2160c1d754163f67b4c559407a94
developer <developer@ip-10-63-36-74.ec2.internal>
2015-04-24 03:57:02 +0000

    forgot a push

--- startTwoWebClients.sh
+++ startTwoWebClients.sh
@@ -3,6 +3,6 @@
 go build -o dibbler
 
 ./dibbler -port=8000 -logfile=port8000.log &
-echo $! > ./port8000Dibbler.pid
+echo $! > ./port8000Dibbler-$!.pid
 ./dibbler -port=8001 -logfile=port8001.log &
-echo $! > ./port8001Dibbler.pid
+echo $! > ./port8001Dibbler-$!.pid
